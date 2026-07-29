fa24742c4ff2310071c3ed9a08910ed13fc7ef35
Vldddddd1 <vldddddd1@yahoo.com>
2026-07-29 20:00:45 +0300

    Frontend Folder Base, Landing Page Started, Preloaded and Updated Fonts

---
+++ frontend/src/assets/themes/colors.ts
@@ -0,0 +1,27 @@
+export const BrandColors = {
+    MainPrimary: '#3A8005', //Same for MAIN/DARK/LIGHT
+    MainSecondary: '#F5EEE0', //Same for MAIN/DARK/ALTERNATIVE
+    LightSecondary: '#202020',
+    AltSecondary: '#14461E',
+}
+
+export const TextColors = {
+    MainGreen: '#3A8005',
+    DarkThemeText: '#F5EEE0',
+    DarkThemeWhite: '#F6F6F6',
+    DarkThemeGrey: '#737373',
+    LightThemeText: '#202020',
+    LightThemeWhite: '#F5EEE0',
+    LightThemeGray: '#666666',
+}
+
+export const BackgroundColors = {
+    DarkThemeBackground: '#202020',
+    LightThemeBackground: '#F5EEE0',
+    CardBackground: '#F6F6F6',
+}
+
+export const shadows = {
+  lightMode: '4px 8px 8px -4px rgba(0, 0, 0, 0.2)',
+  darkMode: '4px 8px 8px -4px rgba(255, 255, 255, 0.2)',
+};
---
+++ frontend/src/assets/themes/theme.ts
@@ -0,0 +1,61 @@
+import { BrandColors, TextColors, BackgroundColors, shadows } from './colors.ts';
+import type { PaletteMode } from '@mui/material';
+
+export const getDesignTokens = (mode: PaletteMode) => ({
+    palette: {
+        mode,
+        ...(mode === 'dark'
+            ? {
+                background: {
+                    default: BackgroundColors.DarkThemeBackground,
+                },
+                primary: {
+                    main: BrandColors.MainPrimary,
+                },
+                text: {
+                    primary: TextColors.DarkThemeText,
+                    secondary: TextColors.DarkThemeGrey,
+                },
+                paper: {
+                    backgroundColor: BackgroundColors.DarkThemeBackground,
+                }
+            }
+            : {
+                background: {
+                    default: BackgroundColors.LightThemeBackground,
+                },
+                primary: {
+                    main: BrandColors.MainPrimary,
+                },
+                text: {
+                    primary: TextColors.LightThemeText,
+                    secondary: TextColors.LightThemeGray,
+                },
+                paper: {
+                    backgroundColor: BackgroundColors.LightThemeBackground,
+                }
+            }),
+    },
+    typography: {
+        fontFamily: [
+            'Sora',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            ].join(','),
+    },
+    components: {
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    boxShadow: mode === 'dark' ? shadows.darkMode : shadows.lightMode,
+                    borderRadius: '8px',
+                },
+            },
+        }
+    }
+});
