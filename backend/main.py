27619ac8e8e1f15975b1ca5b51262a3eee724830
Victor Miron <victor.miron@student.tuiasi.ro>
2026-08-07 18:17:19 +0300

    CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from routers import route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(route.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
