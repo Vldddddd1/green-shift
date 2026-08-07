e0fc3c1541481c26908d50e42f630124b28adfe3
Vladlena <cazacuvladlena03@gmail.com>
2026-08-07 22:32:50 +0300

    connect the frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import route
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(route.router)
 
