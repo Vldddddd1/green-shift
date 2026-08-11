a16b547146be96774a8739d7943b73c1626a87fa
Victor Miron <victor.miron@student.tuiasi.ro>
2026-08-11 14:05:12 +0300

    add dynamic server load updates

--- backend/routers/route.py
+++ backend/routers/route.py
@@ -1,18 +1,26 @@
-
 from fastapi import APIRouter
-from services.carbon_reader import read_carbon_scores
-from services.routing_service import decide_route
+from services.routing_service import decide_route, update_load_after_request
 from models.schemas import RouteResponse, CarbonScoreResponse
 from services.forwarding_service import forward_to_region
+from services.state_service import get_state
 
 router = APIRouter()
 
+
 @router.get("/carbon", response_model=CarbonScoreResponse)
-def get_carbon_scores():
-    return {"zones": read_carbon_scores()}
+def get_carbon():
+    return {"zones": get_state()}
+
 
 @router.get("/route", response_model=RouteResponse)
 def get_route():
     zone, server = decide_route()
     server_response = forward_to_region(server)
-    return {"selected_zone": zone,  "selected_server": server, "server_response": server_response}
+
+    update_load_after_request(zone, server)
+
+    return {
+        "selected_zone": zone,
+        "selected_server": server,
+        "server_response": server_response
+    }
--- backend/services/routing_service.py
+++ backend/services/routing_service.py
@@ -1,4 +1,5 @@
 from services.carbon_reader import read_carbon_scores
+from services.state_service import get_state
 
 
 
@@ -7,7 +8,9 @@ def calculate_score(server_data: dict) -> float:
     return(server_data["carbon_score"] + 0.6 * server_data["current_load"] + 0.1 * server_data["latency"])
 
 def pick_zone(zones: dict) -> str:
-    zone_avg_carbon = { zone_name: sum(s["carbon_score"] for s in servers.values()) / len(servers)
+
+    zone_avg_carbon = { zone_name: sum(s["carbon_score"] for s in servers.values()) / len(servers) 
+                       + 0.3 * (sum(s["current_load"] for s in servers.values()) / len(servers))
                        for zone_name, servers in zones.items()}
     return min(zone_avg_carbon, key=zone_avg_carbon.get)
 
@@ -20,12 +23,25 @@ def pick_server_in_zone(servers: dict) -> str:
     return min(server_score, key = server_score.get)
 
 def decide_route() -> tuple[str, str]:
-    zones = read_carbon_scores()
+
+    zones = get_state()
 
     best_zone = pick_zone(zones)
     best_server = pick_server_in_zone(zones[best_zone])
     
     return best_zone, best_server
+
+def update_load_after_request(selected_zone: str, selected_server: str):
+
+    zones = get_state()
+    for zone_name, servers in zones.items():
+        for server_name, data in servers.items():
+            if server_name == selected_server:
+                data["current_load"] = min(100, data["current_load"] + 5)
+            else:
+                data["current_load"] = max(0, data["current_load"] - 1)
+
+
 
 if __name__ == "__main__": 
     print(decide_route())
